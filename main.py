e2617cbc985ae3e0394e069c018febf538d594ae
Wallace Soares <soareswallace@icloud.com>
2020-06-26 21:33:05 -0300

    start to implement neighborhood radius

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from load_data.load_data import load_from_data
+from neighborhood_classifier.radius_calculator import calculate_instances_neighbours
 
 
 def main():
     file_name = 'data/heart.csv'
-    data = load_from_data(file_name)
+    data = load_from_data(file_name).to_numpy()
+    for instance in data:
+        calculate_instances_neighbours(data, instance)
 
 
 
--- neighborhood_classifier/radius_calculator.py
+++ neighborhood_classifier/radius_calculator.py
@@ -1,9 +1,19 @@
+import numpy as np
+
+
 MIN_DISTANCE = 0
 MAX_DISTANCE = 1
 
 
 def calculate_euclidean_distance(instance_a, instance_b):
-    
+    return np.linalg.norm(instance_b - instance_a)
+
 
 def calculate_instances_neighbours(data, test_instance):
     for data_instance in data:
+        comparison = data_instance == test_instance
+        equal_arrays = comparison.all()
+        if not equal_arrays:
+            distance_between_instances = calculate_euclidean_distance(test_instance, data_instance)
+            if distance_between_instances < 1:
+                print(distance_between_instances)

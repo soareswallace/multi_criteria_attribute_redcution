1a775cb71fe8ca935deb4f60d5e47b1e569b5630
Wallace Soares <soareswallace@icloud.com>
2020-07-15 22:38:16 -0300

    Normalizing data

--- load_data/load_data.py
+++ load_data/load_data.py
@@ -1,5 +1,14 @@
 import pandas as pd
+from sklearn import preprocessing
 
 
 def load_from_data(file_name):
-    return pd.read_csv(file_name, header=None)
+    dataFrame = pd.read_csv(file_name, header=None)
+    number_of_attributes = len(dataFrame[:0].columns) - 1
+    index = 0
+    while index < number_of_attributes:
+        dataFrame[index] = dataFrame[index] / dataFrame[index].max()
+        index+=1
+
+    return dataFrame
+
